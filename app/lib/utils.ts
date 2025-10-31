6cf49720ec1735c629e0ba9797c0fae267404db4
Suprith <suprithsr14@gmail.com>
2025-10-31 19:39:42 +0530

    Resume components

---
+++ app/lib/utils.ts
@@ -0,0 +1,18 @@
+import { twMerge } from "tailwind-merge";
+import {type ClassValue, clsx } from "clsx";
+
+export function cn(...inputs:ClassValue[]){
+    return twMerge(clsx(inputs))
+}
+
+export function formatSize(bytes: number): string {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}
+
+export const generateUUID = () => crypto.randomUUID();
